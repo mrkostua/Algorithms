242ce2842b81f69546664afacf947b457b2153c5
mrkostua <kprysiazhnyi@gmail.com>
2018-03-07 17:29:16 +0100

    finished implementing fun rotateNonPairedSizeMatrix() as in place algorithm

--- src/solutionsForBookCrackingTheCodingInterview/ImageRotation.kt
+++ src/solutionsForBookCrackingTheCodingInterview/ImageRotation.kt
@@ -27,78 +27,125 @@ import java.util.*
  * In other ways with using additional data structure more easier
  */
 
+class ImageRotation {
+    private val defaultByteArray = byteArrayOf(0, 0, 0, 0)
 
-private val defaultByteArray = byteArrayOf(0, 0, 0, 0)
-
-fun rotateTwoDimensionMatrix(imageArray: Array<Array<ByteArray>>, rotationAngle: Int): Array<Array<ByteArray>> {
-    return if (imageArray.size != imageArray[0].size) {
-        rotateMatrixNotInPlaceAlgorithm(imageArray)
-    } else {
-        rotateMatrixInPlaceAlgorithm(imageArray)
-
-    }
-
-
-}
-
-/**
- * first of all it is impossible to do in place if our matrix (xDimension.length != yDimension.length) because we need
- * to change the size of our arrays(which mean creating new array) can't do it in one place.
- * We also can do it recursively.
- */
-private fun rotateMatrixInPlaceAlgorithm(imageArray: Array<Array<ByteArray>>): Array<Array<ByteArray>> {
-    var matrixSize = imageArray.size
-    for (index in 0 until imageArray.size) {
-        if (matrixSize % 2 == 0) {
-            //6x6
+    fun rotateTwoDimensionMatrix(imageArray: Array<Array<ByteArray>>, rotationAngle: Int): Array<Array<ByteArray>> {
+        return if (imageArray.size != imageArray[0].size) {
+            rotateMatrixNotInPlaceAlgorithm(imageArray)
         } else {
-            //5x5
-            var additionalPixel: ByteArray
-            var index1: Int = 0
-            var index2: Int = 0
-            var index3: Int = matrixSize
-            var index4: Int = matrixSize
-            for (column in 0 until matrixSize) {
-                imageArray[]
-                index1++
-                index2++
-                index3--
-                index4--
+            if (imageArray.size % 2 == 0) {
+                rotatePairedSizeMatrix(imageArray)
+            } else {
+                rotateNonPairedSizeMatrix(imageArray)
             }
         }
 
-        matrixSize--
+
     }
 
-}
+    /**
+     * first of all it is impossible to do in place if our matrix (xDimension.length != yDimension.length) because we need
+     * to change the size of our arrays(which mean creating new array) can't do it in one place.
+     * We also can do it recursively.
+     */
+    private fun rotateNonPairedSizeMatrix(imageArray: Array<Array<ByteArray>>): Array<Array<ByteArray>> {
+        println("rotateNonPairedSizeMatrix")
+        var firstIndex = 0
+        var startIndex = 0
+        var matrixSize = imageArray.size
+        var additionalPixel: ByteArray
+        var decreaseIndex: Int
+        var lastIndex: Int
 
-private fun rotateMatrixNotInPlaceAlgorithm(imageArray: Array<Array<ByteArray>>): Array<Array<ByteArray>> {
-    var rotationOfYArray: Array<ByteArray>
-    val resultImageArray = Array(imageArray.size, { Array(imageArray[0].size, { defaultByteArray }) })
-    for (xIndex in 0 until imageArray.size) {
-        rotationOfYArray = imageArray[xIndex]
-        for (yIndex in 0 until imageArray[0].size) {
-            resultImageArray[yIndex][xIndex] = rotationOfYArray[yIndex]
+        var whileSize = imageArray.size
+
+        while (whileSize > 1) {
+            //5x5
+            lastIndex = matrixSize - 1
+            decreaseIndex = matrixSize - 1
+            for (incrementalIndex in startIndex until matrixSize - 1) {
+                println("   incremental index = " + incrementalIndex)
+                println("   decrease index = " + decreaseIndex)
+
+                additionalPixel = imageArray[firstIndex][incrementalIndex]
+
+                //4 move to 1
+                println("move index[" + incrementalIndex + "][" + lastIndex + "] into index[" + firstIndex + "][" + incrementalIndex + "]")
+                imageArray[firstIndex][incrementalIndex] = imageArray[incrementalIndex][lastIndex]
+
+                //3 move to 4
+                println("move index[" + lastIndex + "][" + decreaseIndex + "] into index[" + incrementalIndex + "][" + lastIndex + "]")
+                imageArray[incrementalIndex][lastIndex] = imageArray[lastIndex][decreaseIndex]
+
+                //2 move 3
+                println("move index[" + decreaseIndex + "][" + firstIndex + "] into index[" + lastIndex + "][" + decreaseIndex + "]")
+                imageArray[lastIndex][decreaseIndex] = imageArray[decreaseIndex][firstIndex]
+
+                //1 move to 2
+                println("move index[" + firstIndex + "][" + incrementalIndex + "] into index[" + decreaseIndex + "][" + firstIndex + "]")
+                imageArray[decreaseIndex][firstIndex] = additionalPixel
+
+
+                decreaseIndex--
+
+            }
+            matrixSize--
+            whileSize -= 2
+            firstIndex++
+            startIndex++
+
+            println("\n\nROTATION")
+            for (xArray in imageArray) {
+                for (yArray in xArray) {
+                    print(Arrays.toString(yArray) + "  ")
+                }
+                print("\n")
+            }
+            println("END OF ROTATION \n\n")
+
+
+        }
+        return imageArray
+
+    }
+
+    private fun rotatePairedSizeMatrix(imageArray: Array<Array<ByteArray>>): Array<Array<ByteArray>> {
+        println("rotatePairedSizeMatrix")
+        return imageArray
+    }
+
+    private fun rotateMatrixNotInPlaceAlgorithm(imageArray: Array<Array<ByteArray>>): Array<Array<ByteArray>> {
+        println("rotateMatrixNotInPlaceAlgorithm")
+        var rotationOfYArray: Array<ByteArray>
+        val resultImageArray = Array(imageArray[0].size, { Array(imageArray.size, { defaultByteArray }) })
+        for (xIndex in 0 until imageArray.size) {
+            rotationOfYArray = imageArray[xIndex]
+            for (yIndex in 0 until imageArray[0].size) {
+                resultImageArray[yIndex][xIndex] = rotationOfYArray[yIndex]
+            }
+        }
+        return resultImageArray
+    }
+
+    private fun getRotationAngel(angle: Int): Int {
+        return when {
+            angle <= 90 -> 90
+            angle in 91..180 -> 180
+            angle in 181..270 -> 270
+            else -> 90
         }
     }
-    return resultImageArray
+
 }
 
-private fun getRotationAngel(angle: Int): Int {
-    return when {
-        angle <= 90 -> 90
-        angle in 91..180 -> 180
-        angle in 181..270 -> 270
-        else -> 90
-    }
-}
 
 fun main(args: Array<String>) {
     val positiveArray = byteArrayOf(1, 1, 1, 1)
     val negativeArray = byteArrayOf(0, 0, 0, 0)
 
-    val xDimension = 3
-    val yDimension = 2
+    val xDimension = 5
+    val yDimension = 5
 
     val imageArray = Array(xDimension, { Array(yDimension, { negativeArray }) })
 
@@ -119,7 +166,8 @@ fun main(args: Array<String>) {
         print("\n")
     }
     println("After rotation : \n\n\n ")
-    val resultArray = rotateTwoDimensionMatrix(imageArray)
+    val resultArray = ImageRotation().rotateTwoDimensionMatrix(imageArray, 89)
+
     for (xArray in resultArray) {
         for (yArray in xArray) {
             print(Arrays.toString(yArray) + "  ")
