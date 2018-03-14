0689d7ed7f1daa3663ee1af3545555db52a9805f
mrkostua <kprysiazhnyi@gmail.com>
2018-03-14 16:38:15 +0100

    finished implementing Task 2.1 (algorithm to remove duplicates from an unsorted linked list without using buffer)

--- src/solutionsForBookCrackingTheCodingInterview/LinkedList/UnsortedLinkedListRemoveDuplicates.kt
+++ src/solutionsForBookCrackingTheCodingInterview/LinkedList/UnsortedLinkedListRemoveDuplicates.kt
@@ -2,21 +2,18 @@ package solutionsForBookCrackingTheCodingInterview.LinkedList
 
 import java.util.*
 import solutionsForBookCrackingTheCodingInterview.LinkedList2
-import solutionsForBookCrackingTheCodingInterview.LinkedList2.Node
 
 /**
  * @author Kostiantyn Prysiazhnyi
  * @created on 3/10/2018
+ * "Cracking the Coding Interview" task 2.1
  */
+
 
 /**
  * Write code to remove duplicates from an unsorted linked list.
  * a temporary buffer is not allowed
  **/
-
-
-private val unsortedList = LinkedList<Int>()
-
 
 /**
  * algorithm with using additional buffer
@@ -32,41 +29,46 @@ fun removeDuplicates(list: LinkedList<Int>): LinkedList<Int> {
 }
 
 /**
- * buffer it is a peace of data which remember something that is go be used in the future (gives fast access)
+ * buffer it is a peace of data which remember something that is going to be used in the future (gives fast access)
  * no need to access main data. Some temporary data.
  **/
-fun removeDuplicatesWithoutAdditionalBuffer(head: LinkedList2.Node<Int>?) {
+fun <T : Any> removeDuplicatesWithoutAdditionalBuffer(head: LinkedList2.Node<T>?) {
     println("removeDuplicatesWithoutAdditionalBuffer()")
     if (head == null) {
         println("head is null")
         return
+
     }
-    var currentNode: LinkedList2.Node<Int>? = head
-    var previousNode: LinkedList2.Node<Int>?
-    var loopNode: LinkedList2.Node<Int>?
+    var currentNode: LinkedList2.Node<T>? = head
+    var previousNode: LinkedList2.Node<T>?
+    var loopNode: LinkedList2.Node<T>?
+
     while (currentNode != null) {
         previousNode = currentNode
         loopNode = currentNode.next
         while (loopNode != null) {
             if (currentNode.data == loopNode.data) {
+                //removing loopNode
                 previousNode!!.next = loopNode.next
+                //iteration
+                loopNode = previousNode.next
+
+            } else {
+                //iteration
+                previousNode = loopNode
+                loopNode = loopNode.next
+
             }
-            previousNode = loopNode
-            loopNode = loopNode.next
 
         }
         currentNode = currentNode.next
     }
-
-
-    //we can first sort this array and after just by using iterator check previous and next element for equality
-
-    //search in linked list is not efficient ( it just going form the head to the end and comparing element)
 }
-
 
 fun main(args: Array<String>) {
     val random = Random()
+    val unsortedList = LinkedList<Int>()
+
     println("FIRST version : " + "\n initial unsorted linkedList")
     for (i in 0..20) {
         unsortedList.add(random.nextInt(10))
@@ -80,12 +82,11 @@ fun main(args: Array<String>) {
     println("SECOND version : " + "\n initial unsorted linkedList")
     val linkedList = LinkedList2<Int>()
     for (i in 0..20) {
-        linkedList.addFirst(random.nextInt(10))
+        linkedList.addFirst(random.nextInt(5))
     }
     println(linkedList.toString())
     removeDuplicatesWithoutAdditionalBuffer(linkedList.getHead())
     println("\nafter removing duplicates : ")
-    print(linkedList.toString() + ", ")
-
+    print(linkedList.toString())
 
 }
